9bfce9f05f8b6e2056c32283eb49c0c2f4de0bf0
Michael <Laska>
2020-01-16 18:40:58 -0600

    New updates

--- js/app.js
+++ js/app.js
@@ -1,32 +1,31 @@
 
-//Difine funtion to display charts
+//Define funtion to display charts
+//work in progress - this code needs to be built upon to generate the charts
 function createCharts (recipe) {
-  d3.json("data/sample_data.json").then((data) => {
-    var hits = data.hits;
-    var recipe_names = data.hits.map(l => l.recipe.label);
-    var ingredientsList = data.hits.map(l => l.recipe.ingredientLines);
+  d3.json("data/AllRecipes.json").then((data) => {
+    var hits = data;
+    var recipe_names = data.map(l => l.recipe.label);
+    var ingredientsList = data.map(l => l.recipe.ingredientLines);
 
     console.log(recipe_names);
     console.log(ingredientsList);
-   
-
-
-  } 
+    } 
 )}; 
 
 
 
 
-//Define optionChange to like to html file
+//Define what happens when a recipe is selected from the dropdown
 function optionChanged(recipe)
 {
   console.log("dropdown selection", recipe);
-  createCharts(recipe);
+  //createCharts(recipe);
   //createIngredients(recipe);
   //createGaugeCharts(recipe);
 }
 
 //Initialize Page
+//Function to initialize the dropdown 
 function displayPage()
 {
   console.log("Load Page")
@@ -34,9 +33,9 @@ function displayPage()
   var selector = d3.select("#selDataset");
 
   //Populate the dropdown list
-  d3.json("data/sample_data.json").then((data)=> {
-    var hits = data.hits;
-    var recipe_names = data.hits.map(l => l.recipe.label);
+  d3.json("data/AllRecipes.json").then((data)=> {
+    var hits = data;
+    var recipe_names = data.map(l => l.recipe.label);
     console.log(recipe_names);
     recipe_names.forEach(recipe => {
       selector.append("option").text(recipe).property("value", recipe);
